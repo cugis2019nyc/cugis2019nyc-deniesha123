1badc75ab4c0ea7a860545caabddf63bc7b7bd7f
deniesha123 <54077892+deniesha123@users.noreply.github.com>
2019-08-13 14:59:34 -0400

    Add files via upload

---
+++ addition to day 2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 13:44:02 2019
+
+@author: Columbia Girls
+"""
+chocolate1 = "milk"
+chocolate2 = "dark"
+chocolate3 = "white"
+
+print("chocolate1")
+
+dark = 6
+milk = 5
+white = 8
+
+dark = dark+2
+milk = milk+3
+white = white+2
+
+import math
+dir(math)
+
+math.factorial(7)
+math.sqrt(350)
+math.exp(2)
+math.pow(2.718905609893065,2)
+
+math.pow(35,9)
+35**9
+math.pi
