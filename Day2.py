b768f3a33553b0fd6be46683ff5dc3db0ad3f496
deniesha123 <54077892+deniesha123@users.noreply.github.com>
2019-08-13 13:42:41 -0400

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello deniesha")
+
+def aqua(a,b,c):
+    aqua = a*b*c
+    print("the product of",a,b,"and",c,"is",aqua)
+
+aqua(5,5,5)
+
+#denieshabeason
+
+def star(b,h):
+    star=(1/2)*b*h
+    print("the area of a triangle with base",b,"and height",h,"is",star)
+    
+star(6,6)
+
